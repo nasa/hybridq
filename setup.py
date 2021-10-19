c990fb9ca218bab66d95fb43051d9082fc5cb031
Salvatore Mandrà <salvatore.mandra@gmail.com>
2021-10-19 14:57:02 -0700

    Update version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class MyInstall(DistutilsInstall):
 here = path.abspath(path.dirname(__file__))
 
 # Version
-version = '0.7.7-5'
+version = '0.8.0-dev'
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
