3adc8f724625eb16624683999f35eb6494fe7d7d
Salvatore Mandrà <salvatore.mandra@gmail.com>
2022-01-07 10:15:35 -0800

    Use 'matrix' to reduce the number of workflow files

--- scripts/generate_pytest.sh
+++ scripts/generate_pytest.sh
@@ -22,11 +22,10 @@ PYTHON_VERSIONS="cp37-cp37m cp38-cp38 cp39-cp39"
 TEST_NAMES=$(cat tests/tests.py | grep ^'def test' | sed -e 's/def test_//g' -e 's/__.*//g' | sort -g | uniq)
 
 # For each test, generate yml
-for python_version in $PYTHON_VERSIONS; do
-  for name in $TEST_NAMES; do
-    echo "Generating test for $name (python-${python_version})." >&2
-    cat .github/python-pytest.yml.__base__ | \
-      sed -e "s/\[\[:TESTNAME:\]\]/${name}/g" \
-          -e "s/\[\[::PYTHON_VERSION::\]\]/${python_version}/g" > ".github/workflows/python-pytest_${name}_${python_version}.yml"
-  done
+for name in $TEST_NAMES; do
+  echo "Generating test for $name." >&2
+  cat .github/python-pytest.yml.__base__ | \
+    sed -e "s/\[\[:TESTNAME:\]\]/${name}/g" \
+        -e "s/\[\[:PYTHON_VERSIONS:\]\]/${PYTHON_VERSIONS}/g" > ".github/workflows/python-pytest_${name}.yml"
+  break
 done
