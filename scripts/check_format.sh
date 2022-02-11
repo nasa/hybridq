80a8003e4020a0ba73e26db51545c8dd2b65bceb
Salvatore Mandrà <salvatore.mandra@gmail.com>
2022-02-10 17:50:40 -0800

    Improve code

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -15,6 +15,46 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+function get_files() {
+  git ls-files \
+    $(git rev-parse --show-toplevel) -- \
+      ':!:*.ipynb' \
+      ':!:docs/' \
+      ':!:.github/' \
+      ':!:packages/'
+}
+
+function check_cpp_files() {
+  cat < /dev/stdin | xargs -P8 -l bash -c '\
+    TYPE=$(file $0 | grep -i c++ | wc -l)
+    if [[ $TYPE != 0 ]]; then
+      STATUS=$(clang-format --style=google --output-replacements-xml $0 | wc -l)
+      if [[ $STATUS -gt 3 ]]; then
+        echo $(tput bold setaf 1)[FAILED]$(tput sgr 0) $0 >&2
+        echo $0
+      else
+        echo $(tput bold setaf 2)[PASSED]$(tput sgr 0) $0 >&2
+      fi
+    fi' | sort
+}
+
+function check_py_files() {
+  cat < /dev/stdin | xargs -P8 -l bash -c '\
+    TYPE=$(file $0 | grep -i python | wc -l)
+    if [[ $TYPE != 0 ]]; then
+      STATUS=$(yapf -d --style=google $0 2>&1 >/dev/null)
+      ERR=$?
+      if [[ $ERR != 0 ]]; then
+        if [[ x$STATUS == "x" ]]; then
+          echo $(tput bold setaf 1)[FAILED]$(tput sgr 0) $0 >&2
+          echo $0
+        fi
+      else
+        echo $(tput bold setaf 2)[PASSED]$(tput sgr 0) $0 >&2
+      fi
+    fi' | sort
+}
+
 # Check yapf version
 YAPF_VERSION=0.31.0
 if [[ $(yapf --version | awk '{print$2}') != $YAPF_VERSION ]]; then
@@ -29,29 +69,16 @@ if [[ $(clang-format --version | awk '{print$3}') != $CLANG_VERSION ]]; then
   exit 1
 fi
 
-# Check Python files
-PY_FAILED=""
-for file in $(git ls-files $(git rev-parse --show-toplevel) | \grep '\.py$'); do
-  echo -ne "[......] $file" >&2
-  if [[ $(yapf --style=google -d $file | wc -l) -ne 0 ]]; then
-    PY_FAILED="$file $PY_FAILED"
-    echo -e "\r[FAILED]" >&2
-  else
-    echo -e "\r[PASSED]" >&2
-  fi
-done
+# If no filenames are provided, collect filenames from git tree
+if [[ $# == 0 ]]; then
+  CPP_FAILED=$(get_files | check_cpp_files | tr '\n' ' ')
+  PY_FAILED=$(get_files | check_py_files | tr '\n' ' ')
 
-# Check C++ files
-CPP_FAILED=""
-for file in $(git ls-files $(git rev-parse --show-toplevel) | \grep -E '\.cpp$|\.h$'); do
-  echo -ne "[......] $file" >&2
-  if [[ $(clang-format --style=google --output-replacements-xml $file | wc -l) -gt 3 ]]; then
-    CPP_FAILED="$file $CPP_FAILED"
-    echo -e "\r[FAILED]" >&2
-  else
-    echo -e "\r[PASSED]" >&2
-  fi
-done
+# Otherwise, check provided filenames
+else
+  CPP_FAILED=$(for var in "$@"; do echo $var; done | check_cpp_files | tr '\n' ' ')
+  PY_FAILED=$(for var in "$@"; do echo $var; done | check_py_files | tr '\n' ' ')
+fi
 
 if [[ $PY_FAILED != "" ]]; then
   echo -e "\nFormat of some python files must be fixed. Please use:" >&2
