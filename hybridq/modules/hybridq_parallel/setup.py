07d0dd3c588510c6760a5d7b04890f414a486ebf
Salvatore Mandrà <salvatore.mandra@gmail.com>
2022-11-30 09:03:03 -0800

    Update

--- hybridq/modules/hybridq_parallel/setup.py
+++ hybridq/modules/hybridq_parallel/setup.py
@@ -21,12 +21,45 @@ from os import path
 # Locate right path
 here = path.abspath(path.dirname(__file__))
 
+# Version
+version = '0.1.0'
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 # Get requirements from requirements.txt
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     install_requires = [x.strip() for x in f.readlines()]
 
 setup(
     name='hybridq-parallel',
-    packages=find_packages(exclude=['docs', 'tests']),
+    version=version,
+    description='HybridQ-Parallel is light-weight library to parallelize processes based on concurrent.futures.ProcessPoolExecutor',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nasa/hybridq/modules/hybridq_parallel',
+    author='Salvatore Mandrà',
+    author_email = 'salvatore.mandra@nasa.gov',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: Unix',
+        'Operating System :: MacOS'
+    ],
+    python_requires='>=3.8',
+    keywords=['parallelization', 'multi-threading'],
+    packages=find_packages(exclude=['docs', 'tests', 'tutorials']),
     install_requires=install_requires,
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/nasa/hybridq/issues',
+        'Source': 'https://github.com/nasa/hybridq/modules/hybridq_parallel',
+    },
 )
