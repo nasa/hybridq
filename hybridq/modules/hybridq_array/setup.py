369b24e188f8982a976b858c6333555783e07395
Salvatore Mandrà <salvatore.mandra@gmail.com>
2023-03-13 15:25:43 -0700

    Update `setup.py`

--- hybridq/modules/hybridq_array/setup.py
+++ hybridq/modules/hybridq_array/setup.py
@@ -15,21 +15,56 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
 
+from os import path
 from setuptools import setup, find_packages
 from glob import glob
-from os import path
 
 # Locate right path
 here = path.abspath(path.dirname(__file__))
+
+# Version
+VERSION = '0.1.0'
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 # Get requirements from requirements.txt
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     install_requires = [x.strip() for x in f.readlines()]
 
-setup(name='hybridq-array',
-      packages=find_packages(exclude=['docs', 'tests']),
-      install_requires=install_requires,
-      include_package_data=True,
-      data_files=[('', ['hybridq_array/lib/Makefile'] +
-                   glob('hybridq_array/lib/include/*.h') +
-                   glob('hybridq_array/lib/include/*.cpp'))])
+setup(
+    name='hybridq-array',
+    version=VERSION,
+    description='HybridQ-Array include C++ core implementations of multiple '
+    'functions to manipulate arrays.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nasa/hybridq/modules/hybridq_array',
+    author='Salvatore Mandrà',
+    author_email='salvatore.mandra@nasa.gov',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8', 'Operating System :: Unix',
+        'Operating System :: MacOS'
+    ],
+    python_requires='>=3.8',
+    keywords=['numpy', 'array'],
+    packages=find_packages(exclude=['docs', 'tests', 'tutorials']),
+    install_requires=install_requires,
+    include_package_data=True,
+    data_files=[('', ['hybridq_array/lib/Makefile'] +
+                 glob('hybridq_array/lib/include/*.h') +
+                 glob('hybridq_array/lib/include/*.cpp'))],
+    project_urls={
+        'Bug Reports': 'https://github.com/nasa/hybridq/issues',
+        'Source': 'https://github.com/nasa/hybridq/modules/hybridq_options',
+    },
+)
